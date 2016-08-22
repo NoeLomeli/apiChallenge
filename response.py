3882d15f23d095848a3b19bf10a8e1b77802ef62
Noe Lomeli <noelomeli@student.hartnell.edu>
2016-08-22 10:15:03 -0700

    Step 1 complete

--- response.py
+++ response.py
@@ -3,11 +3,11 @@ import json
 
 
 url = 'http://challenge.code2040.org/api/register'
+#test = 'token'
+tokens = {'token' : '771da4d993f8dc161eca9b1a951bf50e' , 'github' : 'https://github.com/NoeLomeli/apiChallenge'}
 
-#tokens = {'token' : string, 'github' : 'https://github.com/NoeLomeli/apiChallenge'}
+r = requests.post(url, json=tokens)
 
-#r = requests.post(url, json=tokens)
-test = 'http://challenge.code2040.org/api/register'
 
-r = requests.post(url, data = {'token' : test, 'github' : 'https://github.com/NoeLomeli/apiChallenge'})
+#r = requests.post(url, data = {'token' : test, 'github' : 'https://github.com/NoeLomeli/apiChallenge'})
 print(r.text)
