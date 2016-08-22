ff4b4670ce679131dd4151d3fa314cea7d75cb9c
Noe Lomeli <noelomeli@student.hartnell.edu>
2016-08-22 15:40:05 -0700

    Finish adding comments

--- response.py
+++ response.py
@@ -1,13 +1,22 @@
 import requests
 import json
 
+# Noe Lomeli
+# code2040 API challenge
+# August 22, 2016
 
+
+
+# To get started, you are first going to connect to the registration endpoint
+# the endpoint can be found in this URL
 url = 'http://challenge.code2040.org/api/register'
-#test = 'token'
+
+# The registration endpoint expects a JSON dictionary with two keys
+# We will name the first key 'token' and the value is a string that has been provided to us.
+# The second key is 'github' and we assign the url to our github repo
 tokens = {'token' : '771da4d993f8dc161eca9b1a951bf50e' , 'github' : 'https://github.com/NoeLomeli/apiChallenge'}
 
+#  The registration endpoint is going to be expecting you to use POST to send your JSON.
 r = requests.post(url, json=tokens)
 
-
-#r = requests.post(url, data = {'token' : test, 'github' : 'https://github.com/NoeLomeli/apiChallenge'})
 print(r.text)
