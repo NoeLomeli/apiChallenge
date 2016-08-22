6a5948cd9f57bb9fbb80748b2c50e7b729e45694
Noe Lomeli <noelomeli@student.hartnell.edu>
2016-08-21 23:23:56 -0700

    Create string to pass to token

--- response.py
+++ response.py
@@ -1,8 +1,13 @@
 import requests
+import json
+
 
 url = 'http://challenge.code2040.org/api/register'
-tokens = {'token' : 'token', 'github' : 'https://github.com/NoeLomeli/apiChallenge'}
 
-r = requests.post(url, json=tokens)
+#tokens = {'token' : string, 'github' : 'https://github.com/NoeLomeli/apiChallenge'}
 
-#print(r.text)
+#r = requests.post(url, json=tokens)
+test = 'http://challenge.code2040.org/api/register'
+
+r = requests.post(url, data = {'token' : test, 'github' : 'https://github.com/NoeLomeli/apiChallenge'})
+print(r.text)
