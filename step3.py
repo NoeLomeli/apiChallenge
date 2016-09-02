34b4abfe79a08bdb7de3e5e442d969099397cca8
Noe Lomeli <noelomeli@student.hartnell.edu>
2016-09-02 13:47:07 -0700

    Step 3 completed successfully

--- step3.py
+++ step3.py
@@ -10,9 +10,21 @@ token = {"token" : '771da4d993f8dc161eca9b1a951bf50e'}
 req = requests.post(retrieveDictionary, json=token)
 # this is the dictionary that is being returned to us
 print req.text, "\n"
+# We have to convert the dictionary to json
 data = json.loads(req.text)
+# assign the key to value
 value = data['needle']
+# assign the array to array
 array = data['haystack']
-
+# This line iterates through the array to find the needle
 index = array.index(value)
+# print index to confirm the result is correct
 print index
+# this is the url where we will send our result to be validated
+sendIndex = 'http://challenge.code2040.org/api/haystack/validate'
+# We have to create a new dictionary with token and needle keys
+result = {"token" : '771da4d993f8dc161eca9b1a951bf50e', 'needle' : index}
+# Send a post request to upload our dictionary
+reqSend = requests.post(sendIndex, json = result)
+# The result will confirm that step 3 has been completed
+print reqSend.text
