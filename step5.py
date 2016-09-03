82f07abbdddf1573e44ef7034799c8c16c9c41ed
Noe Lomeli <noelomeli@student.hartnell.edu>
2016-09-03 16:41:13 -0700

    Convert back to iso 8601 format

--- step5.py
+++ step5.py
@@ -1,9 +1,11 @@
 import requests
 import json
-import dateutil.parser
-import datetime
 import iso8601
-from datetime import *; from dateutil.relativedelta import *
+from datetime import *
+from dateutil.relativedelta import *
+import dateutil.parser
+import datetime as dt
+
 
 # this is the endpoint that will give us the dictionary
 retrieveDictionary = 'http://challenge.code2040.org/api/dating'
@@ -18,12 +20,25 @@ print req.text, "\n"
 data = json.loads(req.text)
 # assign the datestamp to date
 date = data['datestamp']
+# assign interval to interval variable
 interval = data['interval']
 print date
 print interval
 # We have to format the datestamp in order to be able to add the seconds interval
 date2 = iso8601.parse_date(date)
+# create a new datestamp adding the interval of seconds to the original datestamp
 addSeconds = date2+relativedelta(seconds = interval)
-
-print date2
 print addSeconds
+# Now we have to convert the datestamp back to iso 8601 format
+print unicode(dt.datetime.isoformat(addSeconds)[:-6] + 'Z')
+
+'''
+# Last step is to send the new datestamp to this url
+sendNewList = 'http://challenge.code2040.org/api/dating/validate'
+# We have to create a new dictionary with token and array
+result = {"token" : '771da4d993f8dc161eca9b1a951bf50e', "datestamp" : addSeconds}
+# Send a post request to upload our dictionary
+reqSend = requests.post(sendNewList, json = result)
+# The result will confirm that step 3 has been completed
+print reqSend.text
+'''
