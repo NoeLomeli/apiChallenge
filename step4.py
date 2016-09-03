135e506c2fbc2aeb3dda4950a91851351f177a5d
Noe Lomeli <noelomeli@student.hartnell.edu>
2016-09-03 14:19:02 -0700

    Step 4 completed successfully

--- step4.py
+++ step4.py
@@ -9,7 +9,7 @@ token = {"token" : '771da4d993f8dc161eca9b1a951bf50e'}
 # Next step is to send a post request
 req = requests.post(retrieveDictionary, json=token)
 # this is the dictionary that is being returned to us
-print req.text, "\n"
+#print req.text, "\n"
 # We have to convert the dictionary to json
 data = json.loads(req.text)
 # assign the prefix to value
@@ -18,6 +18,15 @@ value = data['prefix']
 array = data['array']
 # This line iterates through the array to delete the words that contain the prefix
 newlist = [x for x in array if not x.startswith(value)]
-print value
-print newlist
+
+# Last step is to send the new list to this url
+sendNewList = 'http://challenge.code2040.org/api/prefix/validate'
+# We have to create a new dictionary with token and array
+result = {"token" : '771da4d993f8dc161eca9b1a951bf50e', 'array' : newlist}
+# Send a post request to upload our dictionary
+reqSend = requests.post(sendNewList, json = result)
+# The result will confirm that step 3 has been completed
+print reqSend.text
+
+
 # data = json.loads(req.text)
